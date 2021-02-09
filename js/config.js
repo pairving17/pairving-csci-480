84e27fa8053ed43980ab05e5241ee9ede960155b
Philip A. Irving <38569548+pairving17@users.noreply.github.com>
2021-02-09 10:13:53 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolId: '', // e.g. us-east-1_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
